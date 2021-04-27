e1ababdbd97387697a75a9c3d8e42817eb38e5f4
Papp Zsolt <zsolt.papp0606@gmail.com>
2021-04-27 11:51:32 +0200

    8 c.Érje  el,  hogy  alenyíló  lista  és  a bevitelimezőkértékének  változtatása  esetén  a szamol()alprogram kerüljön meghívásra!

--- web/kalkulal.js
+++ web/kalkulal.js
@@ -1,12 +1,16 @@
 function szamol() {
-    var letszam = 1;
-    var ejszaka = 1;
-    var orszag = 'b';
+    var letszam = document.getElementById("letszam").value;
+    var ejszaka = document.getElementById("ejszaka").value;
+    var orszag = document.getElementById("orszag").value;
     var fizetendo;
-
+    var ar = {}
+    ar;{'b'} 5600;
+    ar;{'g'} 6200;
+    ar;{'h'} 5000;
+    ar;{'t'} 5800;
     //--számolás--
-      
+      fizetendo = letszam * ejszaka;
     //------------
-    document.getElementById('eredmeny').value = fizetendo+" Ft";
+    document.getElementById('eredmeny').value = fizetendo.toLocaleString() +" Ft";
     
 }
